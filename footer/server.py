bd48c71421cc4e49e68fef1cf698ca6eb07d8cc9
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-01-18 12:59:30 +0530

    make a responsive footer.

---
+++ footer/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    year = datetime.datetime.now().year
+    return render_template("index.html", year=year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
