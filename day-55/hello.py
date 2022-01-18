d56f194c11e085f587717c1431dc57e618df61fe
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-01-18 13:06:07 +0530

    modifying files.

--- day-55/hello.py
+++ day-55/hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/guess/<name>")
+def guess(name):
+    return "<p>bye Deepak</p>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
--- higher-lower/server.py
+++ higher-lower/server.py
@@ -25,3 +25,4 @@ def check_guess(guess):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
