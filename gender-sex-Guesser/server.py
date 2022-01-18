bf07c3da3d0f852b545e060663fda059d921ab3f
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-01-18 17:29:27 +0530

    complete the guesser

--- gender-sex-Guesser/server.py
+++ gender-sex-Guesser/server.py
@@ -18,8 +18,14 @@ def guess(name):
     age_response = requests.get(age_url)
     age_data = age_response.json()
     age = age_data["age"]
-
     return render_template("guess.html", gender=gender, age=age, name=name)
+
+@app.route("/blog")
+def get_blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_response = requests.get(blog_url)
+    blogs = blog_response.json()
+    return render_template("blog.html", posts=blogs)
 
 if __name__ == "__main__":
     app.run(debug=True)
